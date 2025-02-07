9d6643a212c69c7d16201d2b5bf44fe30ff73097
Alex Threlfo <namey5@hotmail.com>
2025-02-07 16:05:19 +1100

    fix: possible alignment issues in Vec2 functions + misc docs

--- include/dungeon/util.h
+++ include/dungeon/util.h
@@ -11,22 +11,23 @@
 // Clamp value 'x' between 'min' and 'max'.
 #define Clamp(x, min, max) Max((min), Min((max), (x)))
 
-// Fast assign from int8_t[2] 'src' to 'dest'.
+// Copy int8_t[2] value from 'src' to 'dest'.
 static inline void Vec2_Set(int8_t dest[2], const int8_t src[2]) {
-    *(int16_t*)dest = *(int16_t*)src;
+    dest[0] = src[0];
+    dest[1] = src[1];
 }
 
 // Check if 2 int8_t[2] are equal.
 static inline bool Vec2_Equal(const int8_t a[2], const int8_t b[2]) {
-    return *(int16_t*)a == *(int16_t*)b;
+    return a[0] == b[0] && a[1] == b[1];
 }
 
 // Generates a random float in the range of [0,1].
 float Randf32(void);
 // Generates a random float in the range of [min,max].
 float RandRangef32(float min, float max);
-// Generates a random int32_t in the range of [min,max].
-int32_t RandRangei32(int32_t min, int32_t max);
+// Generates a random int32_t in the range of [min,max-1].
+int32_t RandRangei32(int32_t minInclusive, int32_t maxExclusive);
 // Generates a random int32_t index into an array of 'weightCount' size, given 'weights' distribution of probabilities.
 // 'totalWeight' is the total sum of all values in 'weights' - if 0 or less, it will be calculated automatically.
 int32_t RandIndex(int32_t weightCount, const int32_t weights[weightCount], int32_t totalWeight);
