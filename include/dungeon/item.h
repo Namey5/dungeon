018c27ba39398ef1abc88376106814a228d9e305
Alex Threlfo <namey5@hotmail.com>
2025-02-07 21:00:01 +1100

    refactor: move item declaration to its own file

---
+++ include/dungeon/item.h
@@ -0,0 +1,26 @@
+#ifndef __ITEM_H__
+#define __ITEM_H__
+
+typedef enum ItemType {
+    ITEM_FOOD,
+    ITEM_SWORD,
+    ITEM_SHIELD,
+    ITEM_ROPE,
+    ITEM_HOOK,
+    ITEM_ROCK,
+    _ITEM_TYPE_COUNT,
+} ItemType;
+
+static inline const char* ItemType_ToString(const ItemType self) {
+    switch (self) {
+        case ITEM_FOOD: return "FOOD";
+        case ITEM_SWORD: return "SWORD";
+        case ITEM_SHIELD: return "SHIELD";
+        case ITEM_ROPE: return "ROPE";
+        case ITEM_HOOK: return "HOOK";
+        case ITEM_ROCK: return "ROCK";
+        case _ITEM_TYPE_COUNT: return "[ERROR]";
+    }
+}
+
+#endif // __ITEM_H__
--- include/dungeon/player.h
+++ include/dungeon/player.h
@@ -3,6 +3,8 @@
 
 #include <stdint.h>
 
+#include "dungeon/item.h"
+
 typedef struct Player Player;
 
 typedef enum Orientation {
@@ -20,28 +22,6 @@ static inline const char* Orientation_ToString(const Orientation self) {
         case ORIENTATION_SOUTH: return "SOUTH";
         case ORIENTATION_WEST: return "WEST";
         case _ORIENTATION_COUNT: return "[ERROR]";
-    }
-}
-
-typedef enum ItemType {
-    ITEM_FOOD,
-    ITEM_SWORD,
-    ITEM_SHIELD,
-    ITEM_ROPE,
-    ITEM_HOOK,
-    ITEM_ROCK,
-    _ITEM_TYPE_COUNT,
-} ItemType;
-
-static inline const char* ItemType_ToString(const ItemType self) {
-    switch (self) {
-        case ITEM_FOOD: return "FOOD";
-        case ITEM_SWORD: return "SWORD";
-        case ITEM_SHIELD: return "SHIELD";
-        case ITEM_ROPE: return "ROPE";
-        case ITEM_HOOK: return "HOOK";
-        case ITEM_ROCK: return "ROCK";
-        case _ITEM_TYPE_COUNT: return "[ERROR]";
     }
 }
 
